40479018f2bc2cd9b215ff3c86444383171039e5
milad.bourhani <milad.bourhani@accenture.com>
2018-12-22 19:55:10 +0100

    p123

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,7 @@
 module Lib where
 
 import Text.Trifecta
+import Control.Applicative
 
 one = char '1'
 
@@ -26,3 +27,31 @@ testOneEof = do
   print $ parseString (one >> eof) mempty "12"
   print $ parseString (oneTwo >> eof) mempty "12"
   print $ parseString (oneTwo >> eof) mempty "123"
+
+parseFoo :: String -> Result String
+parseFoo = parseString (string "foo") mempty
+{-
+*Main Text.Trifecta> parseFoo "foo"
+Success "foo"
+*Main Text.Trifecta> parseFoo "foobar"
+Success "foo"
+*Main Text.Trifecta> parseFoo "barfoo"
+Failure (ErrInfo {_errDoc = (interactive):1:1: error: expected: "foo"
+barfoo<EOF> 
+^           , _errDeltas = [Columns 0 0]})
+*Main Text.Trifecta> 
+-}
+
+
+p123 :: String -> Result Int
+p123 =
+  parseString (read <$> (p3 <|> p2 <|> p1)) mempty
+
+p1 :: Parser String
+p1 = string "1"
+
+p2 :: Parser String
+p2 = string "12"
+
+p3 :: Parser String
+p3 = string "123"
