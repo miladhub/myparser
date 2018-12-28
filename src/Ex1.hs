7f9a717b41fa0e8b93fd4b1f204cd1c9b24994f5
milad.bourhani <milad.bourhani@accenture.com>
2018-12-28 15:53:49 +0100

    1st parsers chapter exercise

--- src/Ex1.hs
+++ src/Ex1.hs
@@ -5,6 +5,7 @@ module Ex1 where
 import Text.Trifecta
 import Control.Applicative
 import Control.Monad
+import Data.List
 
 data NumberOrString =
     NOSS String
@@ -20,6 +21,33 @@ type Metadata = [NumberOrString]
 data SemVer =
   SemVer Major Minor Patch Release Metadata
   deriving (Show, Eq)
+
+instance Ord SemVer where
+  compare (SemVer maj min pat rel _) (SemVer maj' min' pat' rel' _) =
+    let zipped = zip [maj, min, pat] [maj', min', pat']
+        majMinPatch = mconcat $ fmap (\(f, s) -> compare f s) $ zipped
+        relCompared = compare rel rel'
+        relLengthCompared = compare (length rel') (length rel)
+    in
+      if majMinPatch == EQ then
+        if length rel == 0 then
+          GT
+        else
+          if length rel' == 0 then
+            LT
+          else
+            if relCompared == EQ then
+              relLengthCompared
+            else relCompared
+      else majMinPatch
+
+instance Ord NumberOrString where
+  compare (NOSI _) (NOSS _) = LT
+  compare (NOSI i) (NOSI i') = compare i i'
+  compare (NOSS s) (NOSS s') = compare s s'
+
+psv :: String -> Result SemVer
+psv = parseString parseSemVer mempty
 
 parseSemVer :: Parser SemVer
 parseSemVer = do
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,28 @@
+module Tests where
+
+import Text.Trifecta
+import Test.Hspec
+import Control.Exception (evaluate)
+import Ex1
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Ord instance for SemVer says:" $ do
+    it "SemVer 2 1 0 [] [] < SemVer 2 1 1 [] []" $ do
+      (SemVer 2 1 0 [] [] < SemVer 2 1 1 [] []) `shouldBe` True
+    it "1.0.0-alpha < 1.0.0-alpha.1" $ do
+      checkLessThan "1.0.0-alpha" "1.0.0-alpha.1" `shouldBe` True
+    it "1.0.0-alpha.1 < 1.0.0-alpha.beta" $ do
+      checkLessThan "1.0.0-alpha.1" "1.0.0-alpha.beta" `shouldBe` True
+    it "1.0.0-alpha.beta < 1.0.0-beta.2" $ do
+      checkLessThan "1.0.0-alpha.beta" "1.0.0-beta.2" `shouldBe` True
+    it "1.0.0-beta.2 < 1.0.0-beta.11" $ do
+      checkLessThan "1.0.0-beta.2" "1.0.0-beta.11" `shouldBe` True
+    it "1.0.0-beta.11 < 1.0.0-rc.1" $ do
+      checkLessThan "1.0.0-beta.11" "1.0.0-rc.1" `shouldBe` True
+    it "1.0.0-rc.1 < 1.0.0" $ do
+      checkLessThan "1.0.0-rc.1" "1.0.0" `shouldBe` True
+
+checkLessThan :: String -> String -> Bool
+checkLessThan f s =
+  foldResult (\_ -> undefined) id ((<) <$> psv f <*> psv s)
